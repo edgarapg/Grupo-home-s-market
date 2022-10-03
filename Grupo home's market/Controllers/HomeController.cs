e3d7b2146b31c7d73b798b1b9c3571640fa06c51
LUIS ALEJANDRO GONZALEZ GONZALEZ <luisalejandrogonzalez@ucundinamarca.edu.co>
2022-10-02 19:33:31 -0500

    a

--- Grupo home's market/Controllers/HomeController.cs
+++ Grupo home's market/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Grupo_home_s_market.Models.viewModel;
+using Rotativa;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,11 @@ namespace Grupo_home_s_market.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(String nombre="")
         {
-            return View();
+
+            ViewBag.nombre = nombre;
+            return View(nombre);
         }
 
         public ActionResult About()
@@ -25,6 +29,25 @@ namespace Grupo_home_s_market.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Report()
+        {
+            List<Reporte_VM> lst = new List<Reporte_VM>();
+            lst.Add(new Reporte_VM (){Nombre= "juan", precio = 35, Email ="algo@gmail.com"});
+            lst.Add(new Reporte_VM() { Nombre = "juan1", precio = 36, Email = "algo@gmail.com" });
+            lst.Add(new Reporte_VM() { Nombre = "juan2", precio = 37, Email = "algo@gmail.com" });
+            lst.Add(new Reporte_VM() { Nombre = "juan3", precio = 38, Email = "algo@gmail.com" });
+            lst.Add(new Reporte_VM() { Nombre = "juan4", precio = 95, Email = "algo@gmail.com" });
+            return View(lst );
+
+        }
+        public  ActionResult Print() 
+        {
+
+            return new ActionAsPdf("Report")
+            { FileName = "Test.pdf" };
+
         }
     }
 }
---
+++ Grupo home's market/Models/viewModel/Reporte_VM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Grupo_home_s_market.Models.viewModel
+{
+    public class Reporte_VM
+    {
+        public string Nombre { get; set; }
+        public int precio { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
